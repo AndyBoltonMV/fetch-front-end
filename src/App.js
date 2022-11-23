b4d6c63d515f9be97b8b39281917f1c16210760a
AndyBoltonMV <andrew.bolton@multiverse.io>
2022-11-23 12:40:16 +0000

    ref

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState();
+  const [output, setOutput] = useState();
+
+  async function fetchReq() {
+    try {
+      const response = await fetch("http://localhost:5001/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: name,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      // Move data around my React app
+      setOutput(data.msg);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Fetch in React</h1>
       </header>
+      <main>
+        <h2>This is where the output goes</h2>
+        <input
+          onChange={(event) => setName(event.target.value)}
+          placeholder="Name"
+        />
+        <p>{output}</p>
+        <button onClick={fetchReq}>Make Request</button>
+      </main>
+      <footer>
+        <p>Demonstrated by Andy B</p>
+      </footer>
     </div>
   );
 }
